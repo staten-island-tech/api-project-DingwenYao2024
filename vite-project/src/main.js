8ee7179f6d9aa5b3884b0557da6ce7e5d3c1421b
Dam <dfnex10@gmail.com>
2025-12-22 12:32:01 -0500

    Update main.js

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,24 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+async function getData() {
+  try {
+    //go get data
+    const response = await fetch('https://www.cheapshark.com/api/1.0/games?title=batman');
+    //handle errors
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //makes the response into json data we can use
+      const data = await response.json();
+      console.log(data);
+      document.getElementById("api.response").textContext = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData();
