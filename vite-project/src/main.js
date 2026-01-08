3064f824f2449e06643083a4640c0ad377bd29ca
Dam <dfnex10@gmail.com>
2026-01-08 12:31:33 -0500

    sdf

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,20 +1,37 @@
-import './style.css'
+import "./style.css";
 
 async function getData() {
   try {
-    //go get data
-    const response = await fetch('https://www.cheapshark.com/api/1.0/games?title=batman');
-    //handle errors
-    if (response.status != 200) {
-      throw new Error(response);
-    } else {
-      //makes the response into json data we can use
-      const data = await response.json();
-      console.log(data);
-      document.getElementById("api.response").textContext = data.name;
+    const response = await fetch(
+      "https://www.cheapshark.com/api/1.0/games?title=batman"
+    );
+
+    if (!response.ok) {
+      throw new Error("Network error");
     }
+
+    const data = await response.json();
+    renderCards(data);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 }
+
+function renderCards(games) {
+  const container = document.querySelector(".cards");
+
+  games.forEach((game) => {
+    container.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="card">
+        <h2 class="name">${game.external}</h2>
+        <p>Cheapest: $${game.cheapest}</p>
+      </div>
+      `
+    );
+  });
+}
+
+
 getData();
